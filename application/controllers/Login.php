04aae4694aa20acec91c5ea8af2a60df86988ec7
DIMAS AGUNG SETYOBUDI <agungdimas390@gmail.com>
2023-12-25 15:00:29 +0700

    Update auth password berdasarkan tanggal lahir karyawan

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -23,6 +23,12 @@ class Login extends CI_Controller {
         }
 
         $userdata = json_decode($ambil_data, true);
+        if ($userdata[0]['tanggal_lahir'] != $pass) {
+            # code...
+            echo json_encode(["status" => "error", "message" => "Username/password salah"]);
+            return;
+        }
+        // var_dump($userdata);return;
     
         if ($this->saveUserData($username, $userdata)) {
             echo json_encode(["status" => "success", "message" => "Berhasil menyimpan data ke dalam sesi"]);
